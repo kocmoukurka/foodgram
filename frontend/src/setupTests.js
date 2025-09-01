4f80be7ee933cc5a88fc620e100c04dcaf3a9548
kocmoukurka <kosmoukurka@gmail.com>
2025-09-01 22:33:50 +0300

    Настройка frontend_tests

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -3,3 +3,9 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+if (typeof clearImmediate === 'undefined') {
+  global.clearImmediate = clearTimeout;
+}
+if (typeof setImmediate === 'undefined') {
+  global.setImmediate = setTimeout;
+}
